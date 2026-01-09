67508b48b8caaaa86b44e43793b4d9c9290286e0
ananya615 <patelananya04@gmail.com>
2026-01-09 10:15:33 +0530

    Create Instrument.java

---
+++ src/src/main/java/com/bajaj/trading/src/main/java/com/bajaj/trading/model/Instrument.java
@@ -0,0 +1,8 @@
+package com.bajaj.trading.model;
+
+public class Instrument {
+    public String symbol;
+    public String exchange;
+    public String instrumentType;
+    public double lastTradedPrice;
+}
