bbea86573ec2d7d6a54b219144e10cf636333dbe
ananya615 <patelananya04@gmail.com>
2026-01-09 10:24:54 +0530

    Update InstrumentService.java

--- src/src/main/java/com/bajaj/trading/service/InstrumentService.java
+++ src/src/main/java/com/bajaj/trading/service/InstrumentService.java
@@ -1,1 +1,19 @@
+package com.bajaj.trading.service;
 
+import com.bajaj.trading.model.Instrument;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstrumentService {
+
+    public List<Instrument> getAll() {
+        Instrument tcs = new Instrument();
+        tcs.symbol = "TCS";
+        tcs.exchange = "NSE";
+        tcs.instrumentType = "EQ";
+        tcs.lastTradedPrice = 3650.50;
+        return List.of(tcs);
+    }
+}
