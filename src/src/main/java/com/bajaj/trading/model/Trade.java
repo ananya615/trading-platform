08f40e77b54e80379307e15d4aa4f335e68d9c32
ananya615 <patelananya04@gmail.com>
2026-01-09 10:35:47 +0530

    Create Trade.java

---
+++ src/src/main/java/com/bajaj/trading/model/Trade.java
@@ -0,0 +1,9 @@
+package com.bajaj.trading.model;
+
+public class Trade {
+    public String tradeId;
+    public String orderId;
+    public String symbol;
+    public int quantity;
+    public double price;
+}
