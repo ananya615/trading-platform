0461df5474d642f2fea6dac2fd2182b5343e3722
ananya615 <patelananya04@gmail.com>
2026-01-09 10:09:09 +0530

    Create TradingApplication.java

---
+++ src/src/main/java/com/bajaj/trading/TradingApplication.java
@@ -0,0 +1,14 @@
+
+```java
+package com.bajaj.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingApplication.class, args);
+    }
+}
